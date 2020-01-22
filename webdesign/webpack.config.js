094a311177e5d797b8bb44132f1cdc078585e66c
Raphael Americano <raphaelamericano@MacBook-Pro-de-Claudia.local>
2020-01-21 23:36:11 -0300

    Configuracao do Webpack

--- webdesign/webpack.config.js
+++ webdesign/webpack.config.js
@@ -1,9 +1,62 @@
+const devEnv = process.env.NODE_ENV !== 'production';
 const path = require('path');
+const MiniExtract = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app:'./src/js/index.js',
+        vendor:['jquery', 'popper.js']
+    },
     output: {
         path: path.resolve('dist'),
-        filename: 'main.js'
-    }
+        filename: '[name].[contenthash].js'
+    },
+    optimization:{
+        splitChunks:{
+            chunks: 'all'
+        }
+    },
+    module: {
+        rules:[
+            {
+                test: /\.pug$/,
+                use: 'pug-loader',
+            },
+            {
+                test:/\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader:'babel-loader'
+                }
+            },
+            {
+                test: /\.scss/,
+                use: [
+                    {
+                        loader: MiniExtract.loader
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniExtract({ 
+            filename: devEnv ? '[name].css' : '[name].[hash].css',
+            chunkFilename: devEnv ? '[id].css' : '[id].[hash].css'
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Site name',
+            hash: true,
+            template: 'index.pug',
+            filename: 'index.html',
+            inject: false
+        })
+    ]
 }
