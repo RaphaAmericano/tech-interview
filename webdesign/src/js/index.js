aec3043708cdaea4c83e2f6261ff330577a446d8
Raphael Americano <raphaelamericano@MacBook-Pro-de-Claudia.local>
2020-01-30 14:22:51 -0300

    Header complete

--- webdesign/src/js/index.js
+++ webdesign/src/js/index.js
@@ -2,7 +2,20 @@ import '../scss/main.scss';
 import 'bootstrap';
 
 $(document).ready( () => { 
+
+    let headerHeight = $('header').outerHeight();
+
+    $('.mask').css('padding-top', headerHeight );
+
     $('.navbar-toggler').on('click', () => {
         $('.collapse').collapse('toggle');
+    });
+    $('.collapse').on('show.bs.collapse', () => {
+        $('header').addClass('collapse-xs-extend');
+        $('.navbar-toggler span').removeClass('navbar-toggler-icon').addClass('icon_close');
+    });
+    $('.collapse').on('hidden.bs.collapse', () => {
+        $('header').removeClass('collapse-xs-extend');
+        $('.navbar-toggler span').removeClass('icon_close').addClass('navbar-toggler-icon');
     });
 });
