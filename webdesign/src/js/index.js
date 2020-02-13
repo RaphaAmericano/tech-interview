59960722c77ce3e72e945c1ba0bd457a2418f76e
Raphael Americano <raphaelamericano@MacBook-Pro-de-Claudia.local>
2020-02-13 01:17:53 -0300

    nav with transition and bg

--- webdesign/src/js/index.js
+++ webdesign/src/js/index.js
@@ -18,4 +18,27 @@ $(document).ready( () => {
         $('header').removeClass('collapse-xs-extend');
         $('.navbar-toggler span').removeClass('icon_close').addClass('navbar-toggler-icon');
     });
+
+    var documentSize = document.body.clientHeight;
+
+    window.onscroll = () => {
+        
+        var distanceTop = document.documentElement.scrollTop;
+        var distancePercent = parseInt((distanceTop * 100) / documentSize);
+        
+        if(distancePercent >= 3 ){
+            if(!$('header').hasClass('header-bg')){
+                $('header').addClass('header-bg');
+            }
+        } 
+        if(distancePercent < 3 ){
+            if($('header').hasClass('header-bg')){
+                $('header').removeClass('header-bg');
+            }
+        }
+    }
+
+    
+
+
 });
